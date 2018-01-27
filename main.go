9b0bd53f1968e813842e56ecfc36ff8a15f1eb29
Riya Dennis <riyacv@gmail.com>
2018-01-27 15:34:23 +0000

    Added initial files

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/aws-app/models"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	logging "github.com/sirupsen/logrus"
+)
+
+func main() {
+	db, err := gorm.Open("sqlite3", "userdata.db")
+	if err != nil {
+		logging.Fatalf("Unable to connect to database, got error %s", err)
+	}
+	defer db.Close()
+	db.LogMode(true)
+	db.AutoMigrate(&models.User{}, &models.Follower{}, &models.Photo{}, &models.Comment{})
+
+	r := registerRoutes()
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "5000"
+	}
+
+	log.Printf("Listening on port %s\n", port)
+	r.Run(":" + port)
+}
---
+++ models/structs.go
@@ -0,0 +1,33 @@
+package models
+
+import "time"
+
+//struct that will define user table
+type User struct {
+	ID       uint
+	Email    string
+	Username string
+	Password string
+	FullName string
+}
+
+type Follower struct {
+	UserID     uint `gorm:"unique_index:idx_user_follower"`
+	FollowerID uint `gorm:"unique_index:idx_user_follower"`
+}
+
+type Photo struct {
+	ID        uint
+	UserID    uint
+	Filename  string
+	Caption   string
+	CreatedAt time.Time
+	Likes     uint
+}
+type Comment struct {
+	ID        uint
+	UserID    uint
+	PhotoID   uint
+	Text      string
+	CreatedAt time.Time
+}
