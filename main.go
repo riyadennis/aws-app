ec9398080b527d896772f335f60f4545f097407f
Riya Dennis <riyacv@gmail.com>
2018-04-18 21:06:11 +0100

    moved db functions to a seperate package

--- main.go
+++ main.go
@@ -1,25 +1,16 @@
 package main
 
 import (
-	"log"
 	"os"
 
 	"github.com/aws-app/controllers"
-	"github.com/aws-app/models"
-	"github.com/jinzhu/gorm"
+	"github.com/aws-app/lib"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
 	logging "github.com/sirupsen/logrus"
 )
 
 func main() {
-	db, err := gorm.Open("sqlite3", "userdata.db")
-	if err != nil {
-		logging.Fatalf("Unable to connect to database, got error %s", err)
-	}
-	defer db.Close()
-	db.LogMode(true)
-	db.AutoMigrate(&models.User{}, &models.Follower{}, &models.Photo{}, &models.Comment{})
-
+	lib.SetUpDB()
 	r := controllers.StartUp()
 	port := os.Getenv("PORT")
 
@@ -27,6 +18,6 @@ func main() {
 		port = "5000"
 	}
 
-	log.Printf("Listening on port %s\n", port)
+	logging.Printf("Listening on port %s\n", port)
 	r.Run(":" + port)
 }
